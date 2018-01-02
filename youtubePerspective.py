b93de0daf2142c82911cf277c1cacaed56cb38b5
tydus101 <trr4652@rit.edu>
2018-01-02 18:29:14 -0500

    Added config file functionality in order to store secrets

--- youtubePerspective.py
+++ youtubePerspective.py
@@ -1,12 +1,19 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+import configparser
+# Read config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+# Set global variables
+GOOGLE_CREDENTIALS_KEY = config['API Keys']['google_credentials_key']
 app = Flask(__name__)
+# Debug mode ON!
+app.config['DEBUG'] = True
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Streaks'
+    word = request.args.get('butt')
+    return GOOGLE_CREDENTIALS_KEY
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0:80')
+    app.run(host='0.0.0.0')
